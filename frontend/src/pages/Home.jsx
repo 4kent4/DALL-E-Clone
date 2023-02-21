8c9a2cdea80239fe78bc20c10e0df680cb679b7c
4kent4 <kentfortich.ccs@gmail.com>
2023-02-21 21:02:30 +0800

    Search filter, Dowload, Card component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,15 +8,16 @@ const RenderCards = ({ data, title }) => {
 	}
 
 	return (
-		<h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
+		<h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{title}</h2>
 	);
 };
 
 const Home = () => {
 	const [loading, setLoading] = useState(false);
-	const [allPosts, setAllPost] = useState(null);
-
+	const [allPosts, setAllPosts] = useState(null);
+	const [searchResults, setSearchedResults] = useState(null);
 	const [searchText, setSearchText] = useState("");
+	const [searchTimeout, setSearchTimeout] = useState(null);
 
 	useEffect(() => {
 		const fetchPosts = async () => {
@@ -33,7 +34,7 @@ const Home = () => {
 				if (response.ok) {
 					const result = await response.json();
 
-					setAllPost(result.data.reverse());
+					setAllPosts(result.data.reverse());
 				}
 			} catch (error) {
 				alert(error);
@@ -41,8 +42,24 @@ const Home = () => {
 				setLoading(false);
 			}
 		};
-		fetchPosts;
+		fetchPosts();
 	}, []);
+
+	const handleSearchChange = (e) => {
+		clearTimeout(searchTimeout);
+		setSearchText(e.target.value);
+
+		setSearchTimeout(
+			setTimeout(() => {
+				const searchResult = allPosts.filter(
+					(item) =>
+						item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+						item.prompt.toLowerCase().includes(searchText.toLowerCase())
+				);
+				setSearchedResults(searchResult);
+			}, 500)
+		);
+	};
 
 	return (
 		<section className="max-w-7xl mx-auto">
@@ -57,7 +74,14 @@ const Home = () => {
 			</div>
 
 			<div className="mt-16">
-				<FormField />
+				<FormField
+					labelName="Search posts"
+					type="text"
+					name="text"
+					placeholder="Search posts"
+					value={searchText}
+					handleChange={handleSearchChange}
+				/>
 			</div>
 
 			<div className="mt-10">
@@ -69,15 +93,18 @@ const Home = () => {
 					<>
 						{searchText && (
 							<h2 className="font-medium text-[#666e75] text-xl mb-3">
-								Showing results for{" "}
-								<span className="text-[#222328]">{searchText}</span>
+								Showing Resuls for{" "}
+								<span className="text-[#222328]">{searchText}</span>:
 							</h2>
 						)}
 						<div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
 							{searchText ? (
-								<RenderCards data={[]} title="No search results found" />
+								<RenderCards
+									data={searchResults}
+									title="No Search Results Found"
+								/>
 							) : (
-								<RenderCards data={allPosts} title="No post found" />
+								<RenderCards data={allPosts} title="No Posts Yet" />
 							)}
 						</div>
 					</>
